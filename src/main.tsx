99ffbed3ba7fd5563eff5161a0efbb07db3550ef
Tuan Le <letuanvl03@gmail.com>
2023-11-27 08:55:21 +0700

    Refactor CSS styles and import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import { BrowserRouter as Router} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header.tsx'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Header signIn={false} avatar='' username=''/>
       {/* <Header signIn={true} avatar='src\assets\avatar.png' username='Bông Hub'/> */}
     </Router>
-    
+
   </React.StrictMode>,
 )

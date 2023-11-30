16658a394d061a814ff2f472d7ef693e0551b7d4
chang2694 <21522694@gm.uit.edu.vn>
2023-11-30 14:55:53 +0700

    add forgot password page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router} from 'react-router-dom'
 import './App.css'
-import Header from './components/Header/Header.tsx'
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      // add component here//
+      <ForgotPassword />
     </Router>
 
   </React.StrictMode>,
---
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./forgotPassword.css";
+import Logo from "../../components/Logo";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState<string>("");
+  const handleForgot = () => {
+    console.log("Requesting...");
+  };
+  return (
+    <div className="Forgot">
+      <div className="left">
+        <Logo />
+        <h3>Group 4</h3>
+      </div>
+      <div className="right">
+        <div className="forgot_container">
+          <h2>Forgot password</h2>
+          <p>Please enter the email you use to sign in.</p>
+          <div className="input_container">
+            <input
+              placeholder="Email"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button className="request_btn" onClick={handleForgot}>
+            <Link to="/">Request password reset </Link>
+          </button>
+          <div className="back">
+            <Link to="/sign_in">Back to sign in</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;

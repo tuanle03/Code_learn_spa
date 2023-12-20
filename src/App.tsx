433f245c84e33bee47c602f94a93bbba064dea20
chang2694 <21522694@gm.uit.edu.vn>
2023-12-20 14:13:45 +0700

    Code Pop-up Ask Question

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SignIn from "./Pages/SignIn/SignIn";
 import Home from "./Pages/Home/Home";
 import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import SignUp from "./Pages/SignUp/SignUp";
 import ChangeProfile from "./Pages/ChangeProfile/ChangeProfile";
 import UploadAvatar from "./components/UploadAvatar/UploadAvatar";
-import ChatGPT from "./components/chatGPT/ChatGPT";
+import Question from "./components/Question/Question";
 
 const App = () => {
   return (
@@ -16,9 +16,11 @@ const App = () => {
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/forgot_password" element={<ForgotPassword />} />
         <Route path="/create_account" element={<SignUp />} />
-        <Route path="/profile" element={<ChangeProfile/>} />
-        <Route path="/change_avatar" element={<UploadAvatar/>} />
-        <Route path="/chatgpt" element={<ChatGPT />} />
+        <Route path="/profile" element={<ChangeProfile />} />
+        <Route path="/change_avatar" element={<UploadAvatar />} />
+        <Route path="/ask" element={<Question />} />
+        {/* <Route path="/new_blog" element */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
---
+++ src/components/Question/Question.tsx
@@ -0,0 +1,92 @@
+import React, { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./question.css";
+
+const Question = () => {
+  const [summary, setSummary] = useState("");
+  const [editorState, setEditorState] = useState("");
+  const [tags, setTags] = useState("");
+  const nav = useNavigate();
+
+  const handleSummaryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSummary(e.target.value);
+  };
+
+  const handleEditorChange = (value: string) => {
+    setEditorState(value);
+  };
+
+  const handleTagsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTags(e.target.value);
+  };
+
+  const handlePost = () => {
+    console.log("Post...");
+    nav("/");
+  };
+  const handleClose = () => {
+    nav("/");
+  };
+
+  return (
+    <div className="question-label-container">
+      <h3>Summarize your problem</h3>
+      <input
+        type="text"
+        id="question-input"
+        value={summary}
+        onChange={handleSummaryChange}
+      />
+
+      <h3>Provide details</h3>
+      <div className="question-input-toolbar">
+        <ReactQuill
+          value={editorState}
+          onChange={handleEditorChange}
+          modules={{
+            toolbar: [
+              [{ header: [1, 2, false] }],
+              ["bold", "italic", "underline", "strike", "blockquote"],
+              [{ list: "ordered" }, { list: "bullet" }],
+              ["link", "image", "video"],
+            ],
+          }}
+          formats={[
+            "header",
+            "bold",
+            "italic",
+            "underline",
+            "strike",
+            "blockquote",
+            "list",
+            "bullet",
+            "link",
+            "image",
+            "video",
+          ]}
+        />
+      </div>
+
+      <h3>Tags</h3>
+      <input
+        type="text"
+        id="tags-input"
+        value={tags}
+        onChange={handleTagsChange}
+      />
+      <div className="button">
+        <button className="post" onClick={handlePost}>
+          Post Your Question
+        </button>
+
+        <button className="close" onClick={handleClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
